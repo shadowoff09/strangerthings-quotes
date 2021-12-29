212ec7492495cea83035a9f0c997dfaefd661b27
Diogo Gaspar <diogogaspar931@gmail.com>
2021-12-29 13:53:52 +0000

    initial

--- src/server.js
+++ src/server.js
@@ -1,13 +1,23 @@
 'use strict';
 
 var express = require('express');
+var path = require('path')
 var quotesRepository = require('./quotesRepository');
 var app = express();
 var port = process.env.PORT || 3001;
 
+app.use('/images', express.static('public/images'));
+
+app.use('/css', express.static('public/css'));
+
 app.all('*', function (req, res, next) {
     res.set('Access-Control-Allow-Origin', '*');
     next();
+});
+
+app.get("/", function (request, response){
+    //show this file when the "/" is requested
+    response.sendFile(__dirname+"/index.html");
 });
 
 app.get('/v1/quotes/:num?', function (req, res) {
